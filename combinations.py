4ad426eb01dbb0ce99cb30868eaef8ec4a194915
Yulai202020 <ngmyulai@gmail.com>
2024-05-14 21:27:00 +0400

    first commit

--- combinations.py
+++ combinations.py
@@ -10,11 +10,11 @@ def power_of(card):
 
 def is_flush(hand, desk):
     a = desk + hand
-    g1 = [i[0] for i in a]
+    suits = [i[0] for i in a]
 
-    g3 = ''
-    for i in g1:
-        g3 += i
+    suits_str = ''
+    for i in suits:
+        suits_str += i
 
     from collections import Counter
 
@@ -23,12 +23,12 @@ def is_flush(hand, desk):
         most_common = counter.most_common(1)
         return most_common[0][0] if most_common else None
 
-    most_common = most_common_element(g3)
+    flush_suit = most_common_element(suits_str)
 
     if len(set([i[0] for i in a])) < 4:
         ids = []
         for i in range(7):
-            if a[i][0] == most_common:
+            if a[i][0] == flush_suit:
                 ids.append(i)
 
         if 5 in ids and 6 not in ids:
